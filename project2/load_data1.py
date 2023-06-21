0bca0d1199df71ce8b70b021c8b0ec8c87bbd76e
pragathi <saipragu@gmail.com>
2023-06-21 11:31:26 +0530

    Add project2/load_data1.py

---
+++ project2/load_data1.py
@@ -0,0 +1,9 @@
+
+import os
+from pyspark.sql import SparkSession
+import boto3
+import pandas as pd
+
+spark = SparkSession.builder            .appName('Python Spark Postgresql')            .config("spark.jars.packages", "org.postgresql:postgresql:42.6.0")            .config("spark.sql.execution.arrow.enabled", "true")            .getOrCreate()
+
+print("Hello")
