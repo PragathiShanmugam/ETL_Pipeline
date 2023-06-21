3451a052e44f82e24df66350e86945f18e4858c7
PragathiShanmugam <saipragu@gmail.com>
2023-06-21 10:55:15 +0530

    Delete load_data1.py

--- load_data1.py
+++
@@ -1,9 +0,0 @@
-
-import os
-from pyspark.sql import SparkSession
-import boto3
-import pandas as pd
-
-spark = SparkSession.builder            .appName('Python Spark Postgresql')            .config("spark.jars.packages", "org.postgresql:postgresql:42.6.0")            .config("spark.sql.execution.arrow.enabled", "true")            .getOrCreate()
-
-    
